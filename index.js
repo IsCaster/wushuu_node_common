83bf3140a66e3d4c09d0f12195a77cb38f456cdb
Caster Zeng <caster_zeng@wushuu.com>
2016-10-25 01:17:16 +0800

    feat: unify initial functions

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ exports.Model_regist = require('./lib/Model').Model_regist
 exports.Model_setDB = require('./lib/Model').Model_setDB
 exports.Model_getDB = require('./lib/Model').Model_getDB
 exports.Model_factory = require('./lib/Model').Model_factory
+exports.init = require('./lib/Model').init
