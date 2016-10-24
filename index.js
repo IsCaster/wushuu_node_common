6086384ff26e294cd00a958e0679ae57fcbab277
Caster Zeng <caster_zeng@wushuu.com>
2016-10-24 17:39:58 +0800

    fix: main exports

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 exports.DB = require('./lib/db').DB
 exports.ansyMap = require('./lib/db').ansyMap
+exports.TABLE_TYPE = require('./lib/table').TABLE_TYPE
+exports.DAO = require('./lib/DAO').DAO
+exports.Model_regist = require('./lib/Model').Model_regist
+exports.Model_setDB = require('./lib/Model').Model_setDB
+exports.Model_getDB = require('./lib/Model').Model_getDB
+exports.Model_factory = require('./lib/Model').Model_factory
