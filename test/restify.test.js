9809014528ebe44aa2e1e19d891f26c14ad78f17
xiaodong LI <xiaodong LI>
2016-10-27 18:05:04 +0800

    quick fix

--- test/restify.test.js
+++ test/restify.test.js
@@ -2,14 +2,17 @@ var express = require('express')
 var co = require('co')
 var model_module = require('../lib/Model');
 var db_module = require('../lib/db');
+var pgsqlConf = require('../lib/orm/pgsql.db.config')
 var db_op = require('../lib/restify')
 var request = require('supertest')
 var assert = require('assert')
 var orm = require('orm')
+var should = require('should')
 var check_response = require('../lib/utils').check_response;
 var before_hander
 var after_hander
 var ctx
+var TestTable
 var modelConfigJson = require("../lib/conf/model.conf.example.json")
 var pg_conf = {
     'host': 'WUSHUU-PG',
@@ -23,7 +26,62 @@ var pg_conn_str = "postgres://" + pg_conf.user + ":" + pg_conf.pass + "@" + pg_c
 describe('test common_db_op_test', function() {
     before('init db', function(done) {
         co(function*() {
+            yield pgsqlConf.init(pg_conn_str)
+            var exportName = "test_table"
+            var conf = {
+                "name": "test_table",
+                "type": "simple",
+                "attr": [{
+                    "key": "owner",
+                    "default": 0,
+                    "type": "int"
+                }, {
+                    "key": "type",
+                    "default": "",
+                    "type": "string"
+                }, {
+                    "key": "change",
+                    "default": 0,
+                    "type": "double"
+                }, {
+                    "key": "remark",
+                    "default": "",
+                    "type": "string"
+                }, {
+                    "key": "mall_id",
+                    "default": 0,
+                    "type": "int"
+                }, {
+                    "key": "transaction_time",
+                    "type": "date"
+                }, {
+                    "key": "checked",
+                    "default": 0,
+                    "type": "int"
+                }, {
+                    "key": "code",
+                    "default": 0,
+                    "type": "float",
+                    "size": 4
+                }],
+                "set": false,
+                "PG": true,
+                "timestamp": true
+            }
+            yield pgsqlConf.registerTable(exportName, conf)
+            pgsqlConf.getTables().should.have.property(exportName)
+            TestTable = pgsqlConf.getTables()[exportName]
+            var newEntry = {
+                "owner": "10001",
+                "type": "win",
+                "change": 999999.9999,
+                "transaction_time": new Date()
+            }
+            TestTable.create(newEntry, err => {
+                should.not.exist(err)
+            })
             yield model_module.init(modelConfigJson, pg_conn_str)
+            model_module.registerModel(conf.name, conf)
             var app = express()
             before_hander = function(req, res) {
                 return new Promise(function(resolve, reject) {
@@ -37,19 +95,21 @@ describe('test common_db_op_test', function() {
                     resolve(data)
                 })
             }
-            var list = db_op.list_gen('ap_v2', 'test', before_hander, after_hander)
-            var count = db_op.count_gen('ap_v2', 'test', before_hander)
+            var list = db_op.list_gen(exportName, exportName, before_hander, after_hander)
+            var count = db_op.count_gen(exportName, exportName, before_hander)
             ctx = {
                 express: app
             }
             list(ctx)
             count(ctx)
             done()
+        }).catch(function(err) {
+            console.log('./test/restify.test.js() err' + err.stack)
         })
     })
 
     it('# get list', function(done) {
-        request(ctx.express).get('/test/list')
+        request(ctx.express).get('/test_table/list')
             .query({
                 page_size: 10,
                 or: [{
@@ -63,6 +123,7 @@ describe('test common_db_op_test', function() {
             .end(function(err, res) {
                 check_response(err, res.text).then(function(msg) {
                     assert.equal(1, msg.code)
+                    assert.equal(1, msg.data.length)
                     done()
                 }).catch(function(err) {
                     done(err)
@@ -71,11 +132,11 @@ describe('test common_db_op_test', function() {
     })
 
     it('# get distinct', function(done) {
-        request(ctx.express).get('/test/list')
+        request(ctx.express).get('/test_table/list')
             .query({
                 filter: {
                     distinct: {
-                        code: ""
+                        owner: ""
                     }
                 }
             })
@@ -83,6 +144,7 @@ describe('test common_db_op_test', function() {
             .end(function(err, res) {
                 check_response(err, res.text).then(function(msg) {
                     assert.equal(1, msg.code)
+                    assert.equal('10001', msg.data.owner)
                     done()
                 }).catch(function(err) {
                     done(err)
@@ -91,7 +153,7 @@ describe('test common_db_op_test', function() {
     })
 
     it('# get count api', function(done) {
-        request(ctx.express).get('/test/count')
+        request(ctx.express).get('/test_table/count')
             .query({
                 or: [{
                     owner: orm.not_like('%' + '123' + '%')
@@ -101,10 +163,19 @@ describe('test common_db_op_test', function() {
             .end(function(err, res) {
                 check_response(err, res.text).then(function(msg) {
                     assert.equal(1, msg.code)
+                    assert.equal(1, msg.data)
                     done()
                 }).catch(function(err) {
                     done(err)
                 })
             })
     })
+
+    it('# Clear test Table', function(done) {
+        TestTable.drop(err => {
+            should.not.exist(err)
+            done()
+        })
+    })
+
 })
